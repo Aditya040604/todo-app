4297f065139efab97ad2b4fad445b178c3c5bab3
aditya040604 <adityagurram04@gmail.com>
2025-09-22 12:27:44 +0530

    feat(database): migrate from SQLite to PostgreSQL
    - Refactored users router password change endpoint, removing unnecessary logic
    - Updated requirements.txt to include psycopg2-binary

--- routers/users.py
+++ routers/users.py
@@ -5,7 +5,8 @@ from starlette import status
 from database import SessionLocal
 from sqlalchemy.orm import Session
 from models import Users
-from .auth import get_current_user, bcrypt_context
+from .auth import get_current_user
+from passlib.context import CryptContext
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -25,54 +26,33 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 @router.get("/user", status_code=status.HTTP_200_OK)
 async def get_user(user: user_dependency, db: db_dependency):
-    if user is None or user.get("id") is None:
+    if user is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed"
         )
-    user_model = db.query(Users).filter(Users.id == user.get("id")).first()
-    print(user_model)
-    if user_model is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
-        )
-    user_model = {
-        "first name": user_model.first_name,
-        "last name": user_model.last_name,
-        "email": user_model.email,
-        "username": user_model.username,
-    }
-    return user_model
+    return db.query(Users).filter(Users.id == user.get("id")).first()
 
 
 @router.put("/user/change-password", status_code=status.HTTP_204_NO_CONTENT)
 async def change_password(
     user: user_dependency, db: db_dependency, request: PasswordChangeRequest
 ):
-    if user is None or user.get("id") is None:
+    if user is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed"
         )
     user_model = db.query(Users).filter(Users.id == user.get("id")).first()
-    if user_model is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
-        )
+
     if not bcrypt_context.verify(request.old_password, user_model.hashed_password):
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid old password"
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid old password"
         )
-    if request.old_password == request.new_password:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="New Password must be different",
-        )
-    hashed_new_password = bcrypt_context.hash(request.new_password)
-    db.query(Users).filter(Users.id == user.get("id")).update(
-        {"hashed_password": hashed_new_password}
-    )
+
+    user_model.hashed_password = bcrypt_context.hash(request.new_password)
+    db.add(user_model)
     db.commit()
-    return {"message": "Password changed successfully"}
